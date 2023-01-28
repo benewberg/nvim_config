6d69d706ccaad365a30de314477b753ba3227286
Ben Newberg <ben.newberg@gmail.com>
2023-01-28 13:47:13 -0600

    Change mini_completion to set it not on every `BufEnter` event (default), but on every attach of LSP

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -1,6 +1,10 @@
 local lsp = require 'lspconfig'
 -- lsp.ccls.setup {}  -- default settings; use this for cpp
+local on_attach = function(client, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.MiniCompletion.completefunc_lsp')
+end
 lsp.pylsp.setup {
+    on_attach = on_attach,
     root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),  -- start LSP server at project root or cwd
     cmd = {vim.env.HOME .. '/.virtualenvs/nvim/bin/pylsp'},
     settings = {
