6716f955a095c8cac41810b79ffac154efb094e5
Ben Newberg <ben.newberg@gmail.com>
2025-04-16 10:35:06 -0500

    Add pylsp settings back in for ruff linting

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -2,25 +2,25 @@
 -- lsp.pylsp.setup {
 --     root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),  -- start LSP server at project root or cwd
 --     cmd = {vim.env.HOME .. '/.virtualenvs/nvim/bin/pylsp'},
---     settings = {
---         pylsp = {
---             plugins = {
---                 ruff = {
---                     enabled = true,
---                     executable = vim.env.HOME .. '/.virtualenvs/nvim/bin/ruff',
---                     ignore = {"E501"},  -- ignore line length error
---                     extendSelect = {"W291", "W293"},  -- whitespace warnings
---                     -- TODO add to extendSelect when available: E1120, ...
---                     -- see astral-sh / ruff / issues / 970
---                 },
---             },
---         },
---     },
 -- }
 -- python lsp server
 vim.lsp.config["pylsp"] = {
     cmd = { vim.env.HOME .. '/.virtualenvs/nvim/bin/pylsp' },
     filetypes = { "python" },
+    settings = {
+        pylsp = {
+            plugins = {
+                ruff = {
+                    enabled = true,
+                    executable = vim.env.HOME .. '/.virtualenvs/nvim/bin/ruff',
+                    ignore = {"E501"},  -- ignore line length error
+                    extendSelect = {"W291", "W293"},  -- whitespace warnings
+                    -- TODO add to extendSelect when available: E1120, ...
+                    -- see astral-sh / ruff / issues / 970
+                },
+            },
+        },
+    },
 }
 vim.lsp.enable("pylsp")
 
